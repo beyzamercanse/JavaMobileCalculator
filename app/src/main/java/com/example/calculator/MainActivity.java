2daed8b8980b44e59db8b45779dc28f734a61967
beyzamercan97 <beyzamercan@yonsei.ac.kr>
2021-10-03 18:02:06 +0300

    2nd

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import com.example.calculator.databinding.CalculatorBinding;
+import com.example.calculator.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,26 +12,26 @@ import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private CalculatorBinding binding;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = CalculatorBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.button.setOnClickListener(v -> add());
-        binding.minus.setOnListener(v -> {
-            binding.option.setText("-");
+        binding.buttonEnter.setOnClickListener(v -> add());
+        binding.plus.setOnClickListener(v -> {
+            binding.result.setText("-");
         });
-        binding.plus.setOnClickListener(v-> {
-            binding.option.setText("+");
+        binding.minus.setOnClickListener(v-> {
+            binding.result.setText("+");
         });
         binding.multiply.setOnClickListener(v-> {
-            binding.option.setText("*");
+            binding.result.setText("*");
         });
         binding.divide.setOnClickListener(v-> {
-            binding.option.setText("/");
+            binding.result.setText("/");
         });
 
     }
